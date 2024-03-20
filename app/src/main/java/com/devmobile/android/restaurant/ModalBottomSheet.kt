5b172826ea05279656e7341990f8d8f5bc2227f4
Junior Oliveira <jo2025698@gmail.com>
2024-03-20 12:46:48 -0300

    Bottom sheet added

--- app/src/main/java/com/devmobile/android/restaurant/ModalBottomSheet.kt
+++ app/src/main/java/com/devmobile/android/restaurant/ModalBottomSheet.kt
@@ -8,6 +8,12 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ModalBottomSheet : BottomSheetDialogFragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottomsheet_layout, container, false)
+
     companion object {
         const val TAG = "ModalBottomSheet"
     }
