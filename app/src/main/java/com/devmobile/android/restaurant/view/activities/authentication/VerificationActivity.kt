3405161b0e787cf8999ef315c0102c98e0be7fd1
Junior Oliveira <jo2025698@gmail.com>
2024-06-27 20:45:36 -0300

    refactor: text code size changed

--- app/src/main/java/com/devmobile/android/restaurant/view/activities/authentication/VerificationActivity.kt
+++ app/src/main/java/com/devmobile/android/restaurant/view/activities/authentication/VerificationActivity.kt
@@ -3,6 +3,7 @@ package com.devmobile.android.restaurant.view.activities.authentication
 import android.os.Bundle
 import android.text.InputType
 import android.view.Gravity
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.devmobile.android.restaurant.databinding.ActivityVerificationCodeBinding
@@ -16,28 +17,43 @@ class VerificationActivity : AppCompatActivity() {
         _verificationBinding = ActivityVerificationCodeBinding.inflate(layoutInflater)
         setContentView(_verificationBinding.root)
 
-        _verificationBinding.code1.textInputEditText.requestFocus()
+        with(_verificationBinding) {
 
-        _verificationBinding.code1.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
-        _verificationBinding.code2.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
-        _verificationBinding.code3.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
-        _verificationBinding.code4.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
-        _verificationBinding.code5.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
-        _verificationBinding.code6.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            // Init with focus
+            code1.textInputEditText.requestFocus()
 
-        _verificationBinding.code1.textInputEditText.gravity = Gravity.CENTER
-        _verificationBinding.code2.textInputEditText.gravity = Gravity.CENTER
-        _verificationBinding.code3.textInputEditText.gravity = Gravity.CENTER
-        _verificationBinding.code4.textInputEditText.gravity = Gravity.CENTER
-        _verificationBinding.code5.textInputEditText.gravity = Gravity.CENTER
-        _verificationBinding.code6.textInputEditText.gravity = Gravity.CENTER
+            // InputType
+            code1.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            code2.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            code3.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            code4.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            code5.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            code6.textInputEditText.inputType = InputType.TYPE_CLASS_NUMBER
 
-        _verificationBinding.code1.textInputEditText.maxLength(1)
-        _verificationBinding.code2.textInputEditText.maxLength(1)
-        _verificationBinding.code3.textInputEditText.maxLength(1)
-        _verificationBinding.code4.textInputEditText.maxLength(1)
-        _verificationBinding.code5.textInputEditText.maxLength(1)
-        _verificationBinding.code6.textInputEditText.maxLength(1)
+            // Text alignment
+            code1.textInputEditText.gravity = Gravity.CENTER
+            code2.textInputEditText.gravity = Gravity.CENTER
+            code3.textInputEditText.gravity = Gravity.CENTER
+            code4.textInputEditText.gravity = Gravity.CENTER
+            code5.textInputEditText.gravity = Gravity.CENTER
+            code6.textInputEditText.gravity = Gravity.CENTER
+
+            // Max chars in text
+            code1.textInputEditText.maxLength(1)
+            code2.textInputEditText.maxLength(1)
+            code3.textInputEditText.maxLength(1)
+            code4.textInputEditText.maxLength(1)
+            code5.textInputEditText.maxLength(1)
+            code6.textInputEditText.maxLength(1)
+
+            // Layout text size
+            code1.textInputForm.layoutParams.height = 300
+            code2.textInputForm.layoutParams.height = 300
+            code3.textInputForm.layoutParams.height = 300
+            code4.textInputForm.layoutParams.height = 300
+            code5.textInputForm.layoutParams.height = 300
+            code6.textInputForm.layoutParams.height = 300
+        }
 
         setObservables()
     }
@@ -46,42 +62,36 @@ class VerificationActivity : AppCompatActivity() {
         with(_verificationBinding) {
 
             code1.textInputEditText.doAfterTextChanged {
-                if (it != null) {
-                    if (it.length == 1)
-                        code2.textInputEditText.requestFocus()
-                }
+                focus(code1.textInputEditText)
             }
 
             code2.textInputEditText.doAfterTextChanged {
-                if (it != null) {
-                    if (it.length == 1)
-                        code3.textInputEditText.requestFocus()
-                }
+                focus(code2.textInputEditText)
             }
 
             code3.textInputEditText.doAfterTextChanged {
-                if (it != null) {
-                    if (it.length == 1)
-                        code4.textInputEditText.requestFocus()
-                }
+                focus(code3.textInputEditText)
             }
 
             code4.textInputEditText.doAfterTextChanged {
-                if (it != null) {
-                    if (it.length == 1)
-                        code5.textInputEditText.requestFocus()
-                }
+                focus(code4.textInputEditText)
             }
 
             code5.textInputEditText.doAfterTextChanged {
-                if (it != null) {
-                    if (it.length == 1)
-                        code6.textInputEditText.requestFocus()
-                }
+                focus(code5.textInputEditText)
             }
 
             code6.textInputEditText.doAfterTextChanged {
-                // check if code is valid
+                // check if code is valid on ViewModel
+            }
+        }
+    }
+
+    private fun <T : EditText> focus(text: T?) {
+
+        if (text != null) {
+            if (text.length() == 1) {
+                text.requestFocus()
             }
         }
     }
