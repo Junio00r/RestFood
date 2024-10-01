7b400914a4b09b7a63402b1eb1f9ebaba48b7cdb
Junior Oliveira <jo2025698@gmail.com>
2024-10-01 10:24:41 -0300

    build: added activity-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics:1.7.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
     implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation(platform("androidx.compose:compose-bom:2024.09.01"))
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.1")
