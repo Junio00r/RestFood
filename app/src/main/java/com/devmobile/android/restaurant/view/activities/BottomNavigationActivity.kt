8e9a9db96195b47d884d506639be6bb3a98d61b5
Junior Oliveira <jo2025698@gmail.com>
2024-10-10 18:05:22 -0300

    feat: add navigation bottom

--- app/src/main/java/com/devmobile/android/restaurant/view/activities/BottomNavigationActivity.kt
+++ app/src/main/java/com/devmobile/android/restaurant/view/activities/BottomNavigationActivity.kt
@@ -1,48 +1,114 @@
 package com.devmobile.android.restaurant.view.activities
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.devmobile.android.restaurant.R
-import com.devmobile.android.restaurant.databinding.ActivityHomeBinding
+import com.devmobile.android.restaurant.databinding.ActivityBottomNavigationBinding
+import java.util.LinkedList
 
 class BottomNavigationActivity : AppCompatActivity() {
 
-    //    private val repository =
-//    private val _viewModel  =
-    private val _binding: ActivityHomeBinding by lazy {
-        ActivityHomeBinding.inflate(layoutInflater)
+    private val _binding: ActivityBottomNavigationBinding by lazy {
+        ActivityBottomNavigationBinding.inflate(layoutInflater)
     }
+    private val _navigationMenuStack: LinkedList<Int> = LinkedList(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (savedInstanceState == null) {
+
+            subscribeObservers()
+            _navigationMenuStack.add(0)
+            startFragment(HomeFragment.getInstance(), HomeFragment.FRAGMENT_TAG)
+        }
         setContentView(_binding.root)
-        subscribeObservers()
-    }
-
-    private fun drawViews() {
-
     }
 
     private fun subscribeObservers() {
 
-        _binding.bottomNavigationView.setOnItemReselectedListener { item ->
+        _binding.bottomNavigationView.setOnItemSelectedListener { item ->
 
-            when(item.itemId) {
+            when (item.itemId) {
+
                 R.id.menu_screen_1 -> {
-                    Log.i("HOME_SCREEN", "Menu item 1\n")
+
+                    startFragment(HomeFragment.getInstance(), HomeFragment.FRAGMENT_TAG)
+                    _navigationMenuStack.add(0)
+                    true
                 }
+
                 R.id.menu_screen_2 -> {
-                    Log.i("HOME_SCREEN", "Menu item 2\n")
+
+                    startFragment(QrCodeFragment.getInstance(), QrCodeFragment.FRAGMENT_TAG)
+                    _navigationMenuStack.add(1)
+                    true
                 }
+
                 R.id.menu_screen_3 -> {
-                    Log.i("HOME_SCREEN", "Menu item 3\n")
+
+                    startFragment(MapFragment.getInstance(), MapFragment.FRAGMENT_TAG)
+                    _navigationMenuStack.add(2)
+                    true
                 }
+
                 R.id.menu_screen_4 -> {
-                    Log.i("HOME_SCREEN", "Menu item 4\n")
+
+                    startFragment(AccountFragment.getInstance(), AccountFragment.FRAGMENT_TAG)
+                    _navigationMenuStack.add(3)
+                    true
                 }
+
+                else -> {
+                    false
+                }
+            }
+
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+
+            if (supportFragmentManager.fragments.isEmpty() && _navigationMenuStack.size == 0) {
+                finish()
+            } else if (
+                _navigationMenuStack.size >= 1
+                && supportFragmentManager.backStackEntryCount < _navigationMenuStack.size
+            ) {
+
+                _navigationMenuStack.removeLast()
+                _binding.bottomNavigationView.menu[_navigationMenuStack.last()].isChecked = true
             }
         }
     }
+
+    private inline fun <reified F : Fragment> startFragment(fragment: F, tag: String) {
+
+        if (supportFragmentManager.backStackEntryCount > 0) {
+
+            if (supportFragmentManager.fragments[supportFragmentManager.fragments.lastIndex].tag != tag) {
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container_view, fragment, tag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_MATCH_ACTIVITY_OPEN)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(tag)
+                    .commit()
+            } else {
+                _navigationMenuStack.removeLast()
+            }
+
+        } else {
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, fragment, tag)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_MATCH_ACTIVITY_OPEN)
+                .setReorderingAllowed(true)
+                .addToBackStack(tag)
+                .commit()
+        }
+    }
 }
+
