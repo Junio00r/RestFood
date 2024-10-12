5ff03d5d076f897c02cf1a4833422fa89676ce1e
Junior Oliveira <jo2025698@gmail.com>
2024-10-12 20:42:26 -0300

    feat: add fragment index persistence

--- app/src/main/java/com/devmobile/android/restaurant/view/activities/BottomNavigationActivity.kt
+++ app/src/main/java/com/devmobile/android/restaurant/view/activities/BottomNavigationActivity.kt
@@ -1,12 +1,16 @@
 package com.devmobile.android.restaurant.view.activities
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.distinctUntilChanged
 import com.devmobile.android.restaurant.R
 import com.devmobile.android.restaurant.databinding.ActivityBottomNavigationBinding
+import com.devmobile.android.restaurant.viewmodel.BottomNavigationViewModel
 import java.util.LinkedList
 
 class BottomNavigationActivity : AppCompatActivity() {
@@ -14,7 +18,16 @@ class BottomNavigationActivity : AppCompatActivity() {
     private val _binding: ActivityBottomNavigationBinding by lazy {
         ActivityBottomNavigationBinding.inflate(layoutInflater)
     }
+    private val _viewModel: BottomNavigationViewModel by viewModels {
+        BottomNavigationViewModel.provideFactory(this@BottomNavigationActivity, null)
+    }
     private val _navigationMenuStack: LinkedList<Int> = LinkedList(emptyList())
+
+    // data
+    private val homeFragment = HomeFragment()
+    private val qrCodeFragment = QrCodeFragment()
+    private val mapFragment = MapFragment()
+    private val accountFragment = AccountFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +36,7 @@ class BottomNavigationActivity : AppCompatActivity() {
 
             subscribeObservers()
             _navigationMenuStack.add(0)
-            startFragment(HomeFragment.getInstance(), HomeFragment.FRAGMENT_TAG)
+            startFragment(homeFragment, HomeFragment.FRAGMENT_TAG)
         }
         setContentView(_binding.root)
     }
@@ -36,55 +49,54 @@ class BottomNavigationActivity : AppCompatActivity() {
 
                 R.id.menu_screen_1 -> {
 
-                    startFragment(HomeFragment.getInstance(), HomeFragment.FRAGMENT_TAG)
-                    _navigationMenuStack.add(0)
+                    startFragment(homeFragment, HomeFragment.FRAGMENT_TAG)
+                    _viewModel.updateIndex(_binding.bottomNavigationView.menu.children.indexOf(item))
                     true
                 }
 
                 R.id.menu_screen_2 -> {
 
-                    startFragment(QrCodeFragment.getInstance(), QrCodeFragment.FRAGMENT_TAG)
-                    _navigationMenuStack.add(1)
+                    startFragment(qrCodeFragment, QrCodeFragment.FRAGMENT_TAG)
+                    _viewModel.updateIndex(_binding.bottomNavigationView.menu.children.indexOf(item))
                     true
                 }
 
                 R.id.menu_screen_3 -> {
 
-                    startFragment(MapFragment.getInstance(), MapFragment.FRAGMENT_TAG)
-                    _navigationMenuStack.add(2)
+                    startFragment(mapFragment, MapFragment.FRAGMENT_TAG)
+                    _viewModel.updateIndex(_binding.bottomNavigationView.menu.children.indexOf(item))
                     true
                 }
 
                 R.id.menu_screen_4 -> {
 
-                    startFragment(AccountFragment.getInstance(), AccountFragment.FRAGMENT_TAG)
-                    _navigationMenuStack.add(3)
+                    startFragment(accountFragment, AccountFragment.FRAGMENT_TAG)
+                    _viewModel.updateIndex(_binding.bottomNavigationView.menu.children.indexOf(item))
                     true
                 }
 
                 else -> {
+
                     false
                 }
             }
 
         }
 
+        _viewModel.indexFragment.distinctUntilChanged().observe(this@BottomNavigationActivity) { index ->
+
+                _binding.bottomNavigationView.menu[index].isChecked = true
+        }
+
         supportFragmentManager.addOnBackStackChangedListener {
 
             if (supportFragmentManager.fragments.isEmpty() && _navigationMenuStack.size == 0) {
                 finish()
-            } else if (
-                _navigationMenuStack.size >= 1
-                && supportFragmentManager.backStackEntryCount < _navigationMenuStack.size
-            ) {
-
-                _navigationMenuStack.removeLast()
-                _binding.bottomNavigationView.menu[_navigationMenuStack.last()].isChecked = true
             }
         }
     }
 
-    private inline fun <reified F : Fragment> startFragment(fragment: F, tag: String) {
+    private fun <F : Fragment> startFragment(fragment: F, tag: String) {
 
         if (supportFragmentManager.backStackEntryCount > 0) {
 
@@ -93,11 +105,7 @@ class BottomNavigationActivity : AppCompatActivity() {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container_view, fragment, tag)
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_MATCH_ACTIVITY_OPEN)
-                    .setReorderingAllowed(true)
-                    .addToBackStack(tag)
-                    .commit()
-            } else {
-                _navigationMenuStack.removeLast()
+                    .setReorderingAllowed(true).addToBackStack(tag).commit()
             }
 
         } else {
@@ -105,10 +113,7 @@ class BottomNavigationActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container_view, fragment, tag)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_MATCH_ACTIVITY_OPEN)
-                .setReorderingAllowed(true)
-                .addToBackStack(tag)
-                .commit()
+                .setReorderingAllowed(true).addToBackStack(tag).commit()
         }
     }
 }
-
