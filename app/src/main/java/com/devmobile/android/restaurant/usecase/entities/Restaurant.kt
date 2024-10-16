d02140a3ef68e8460b27d1c7398d06271481db67
Junior Oliveira <jo2025698@gmail.com>
2024-10-16 09:48:37 -0300

    build: create restaurant entity

--- app/src/main/java/com/devmobile/android/restaurant/usecase/entities/Restaurant.kt
+++ app/src/main/java/com/devmobile/android/restaurant/usecase/entities/Restaurant.kt
@@ -1,4 +1,14 @@
 package com.devmobile.android.restaurant.usecase.entities
 
-class Restaurant {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "restaurants")
+data class Restaurant(
+    @PrimaryKey(autoGenerate = true) val id: Long? = null,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "street") var street: String,
+    @ColumnInfo(name = "city") var city: String,
+    @ColumnInfo(name = "postalCode") var postalCode: String,
+)
