c14cca9a4d9762dc95a6c638100899d4d4ea2941
Junior Oliveira <jo2025698@gmail.com>
2024-10-20 13:08:01 -0300

    feat: change query for restaurants

---
+++ app/src/main/java/com/devmobile/android/restaurant/model/repository/datasource/local/IRestaurantDao.kt
@@ -0,0 +1,27 @@
+package com.devmobile.android.restaurant.model.repository.datasource.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.devmobile.android.restaurant.usecase.entities.Restaurant
+
+@Dao
+interface IRestaurantDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAll(restaurant: List<Restaurant>)
+
+    @Query("SELECT * from restaurants WHERE id = :id")
+    suspend fun getById(id: Long): Restaurant?
+
+    @Query("SELECT * from restaurants")
+    suspend fun getAll(): List<Restaurant?>
+
+    @Query("SELECT name FROM restaurants WHERE name LIKE '%' || :searchName || '%' LIMIT :limit")
+    suspend fun getNameMatches(searchName: String, limit: Int = 20): List<RestaurantTuple>
+
+
+    data class RestaurantTuple(@ColumnInfo(name = "name") val name: String)
+}
