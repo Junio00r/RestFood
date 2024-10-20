e193241b513fbec9b3db773ed437b52239929388
Junior Oliveira <jo2025698@gmail.com>
2024-10-20 18:48:36 -0300

    feat: add any query and rename class

--- app/src/main/java/com/devmobile/android/restaurant/model/repository/datasource/local/IFetches.kt
+++ app/src/main/java/com/devmobile/android/restaurant/model/repository/datasource/local/IFetches.kt
@@ -1,15 +1,26 @@
 package com.devmobile.android.restaurant.model.repository.datasource.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.devmobile.android.restaurant.usecase.Fetches
+import com.devmobile.android.restaurant.usecase.Fetch
 
 @Dao
 interface IFetches {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(fetches: List<Fetch>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(fetch: Fetch)
+
     @Query("DELETE FROM fetches")
     suspend fun clearCache()
 
+    @Query("DELETE FROM fetches WHERE fetch = :name")
+    suspend fun removeFetchByName(name: String)
+
     @Query("SELECT fetch FROM fetches")
-    suspend fun getFetchesCache(): List<Fetches>
+    suspend fun getCachedFetches(): List<String>
 }
--- app/src/main/java/com/devmobile/android/restaurant/usecase/Fetch.kt
+++ app/src/main/java/com/devmobile/android/restaurant/usecase/Fetch.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "fetches")
-data class Fetches(
+data class Fetch(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "fetch") val fetchName: String
 )
