d483234c1197fa71bc8c46c627b1ae8ad3d3a04b
Junior Oliveira <jo2025698@gmail.com>
2024-10-20 13:05:06 -0300

    feat: add fakerestaurants

--- app/src/main/java/com/devmobile/android/restaurant/model/repository/datasource/local/DatabaseSimulator.kt
+++ app/src/main/java/com/devmobile/android/restaurant/model/repository/datasource/local/DatabaseSimulator.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.devmobile.android.restaurant.R
 import com.devmobile.android.restaurant.model.datasource.local.RestaurantLocalDatabase
 import com.devmobile.android.restaurant.model.datasource.local.entities.Food
+import com.devmobile.android.restaurant.usecase.entities.Restaurant
 import com.devmobile.android.restaurant.usecase.enums.FoodSection
 import com.devmobile.android.restaurant.usecase.enums.TempoPreparo
 
@@ -11,6 +12,378 @@ import com.devmobile.android.restaurant.usecase.enums.TempoPreparo
 class DatabaseSimulator {
 
     companion object {
+
+        suspend fun addRestaurants(context: Context) {
+            val restaurants = ArrayList<Restaurant>()
+            val restaurantDao = RestaurantLocalDatabase.getInstance(context).getRestaurantDao()
+
+            if (restaurantDao.getAll().isEmpty()) {
+
+                restaurants.addAll(
+                    listOf(
+                        Restaurant(
+                            id = 0,
+                            name = "Casa do Camarao",
+                            street = "Rua do camarao, 003",
+                            city = "Rio Grande do Sul",
+                            postalCode = "89433-439"
+                        ),
+                        Restaurant(
+                            id = 1,
+                            name = "Bella Italia",
+                            street = "Avenida das Flores, 123",
+                            city = "São Paulo",
+                            postalCode = "01234-567"
+                        ),
+                        Restaurant(
+                            id = 2,
+                            name = "Sushi House",
+                            street = "Rua dos Sushis, 456",
+                            city = "Rio de Janeiro",
+                            postalCode = "02345-678"
+                        ),
+                        Restaurant(
+                            id = 3,
+                            name = "Pizzeria Napoli",
+                            street = "Rua da Pizza, 789",
+                            city = "Belo Horizonte",
+                            postalCode = "03456-789"
+                        ),
+                        Restaurant(
+                            id = 4,
+                            name = "Churrascaria Gaúcha",
+                            street = "Rua do Churrasco, 101",
+                            city = "Porto Alegre",
+                            postalCode = "04567-890"
+                        ),
+                        Restaurant(
+                            id = 5,
+                            name = "Café do Mercado",
+                            street = "Rua das Frutas, 202",
+                            city = "Curitiba",
+                            postalCode = "05678-901"
+                        ),
+                        Restaurant(
+                            id = 6,
+                            name = "Restaurante Mar & Terra",
+                            street = "Avenida do Litoral, 303",
+                            city = "Salvador",
+                            postalCode = "06789-012"
+                        ),
+                        Restaurant(
+                            id = 7,
+                            name = "Bistrô Parisiense",
+                            street = "Rua de Paris, 404",
+                            city = "Florianópolis",
+                            postalCode = "07890-123"
+                        ),
+                        Restaurant(
+                            id = 8,
+                            name = "Cantina do Italiano",
+                            street = "Rua Roma, 505",
+                            city = "Recife",
+                            postalCode = "08901-234"
+                        ),
+                        Restaurant(
+                            id = 9,
+                            name = "Taco Loco",
+                            street = "Avenida dos Tacos, 606",
+                            city = "Brasília",
+                            postalCode = "09012-345"
+                        ),
+                        Restaurant(
+                            id = 10,
+                            name = "Pub da Esquina",
+                            street = "Rua das Cervejas, 707",
+                            city = "Goiânia",
+                            postalCode = "10123-456"
+                        ),
+                        Restaurant(
+                            id = 11,
+                            name = "Cozinha Mineira",
+                            street = "Rua das Minas, 808",
+                            city = "Uberlândia",
+                            postalCode = "11234-567"
+                        ),
+                        Restaurant(
+                            id = 12,
+                            name = "Grill do Chef",
+                            street = "Rua do Grelhado, 909",
+                            city = "Natal",
+                            postalCode = "12345-678"
+                        ),
+                        Restaurant(
+                            id = 13,
+                            name = "Vegan Garden",
+                            street = "Rua Verde, 111",
+                            city = "São Luís",
+                            postalCode = "23456-789"
+                        ),
+                        Restaurant(
+                            id = 14,
+                            name = "Delícias do Nordeste",
+                            street = "Avenida do Nordeste, 222",
+                            city = "João Pessoa",
+                            postalCode = "34567-890"
+                        ),
+                        Restaurant(
+                            id = 15,
+                            name = "Sorveteria Gelato",
+                            street = "Rua dos Gelados, 333",
+                            city = "Maceió",
+                            postalCode = "45678-901"
+                        ),
+                        Restaurant(
+                            id = 16,
+                            name = "Burguer & Beer",
+                            street = "Rua do Hambúrguer, 444",
+                            city = "Vitória",
+                            postalCode = "56789-012"
+                        ),
+                        Restaurant(
+                            id = 17,
+                            name = "Pão de Queijo & Cia",
+                            street = "Avenida do Pão, 555",
+                            city = "Campo Grande",
+                            postalCode = "67890-123"
+                        ),
+                        Restaurant(
+                            id = 18,
+                            name = "Café Colonial",
+                            street = "Rua dos Cafés, 666",
+                            city = "Caxias do Sul",
+                            postalCode = "78901-234"
+                        ),
+                        Restaurant(
+                            id = 19,
+                            name = "Chá da Tarde",
+                            street = "Rua das Infusões, 777",
+                            city = "Teresina",
+                            postalCode = "89012-345"
+                        ),
+                        Restaurant(
+                            id = 20,
+                            name = "Taberna Medieval",
+                            street = "Rua dos Cavaleiros, 888",
+                            city = "Aracaju",
+                            postalCode = "90123-456"
+                        ),
+                        Restaurant(
+                            id = 21,
+                            name = "Restaurante Fusion",
+                            street = "Avenida das Misturas, 999",
+                            city = "Londrina",
+                            postalCode = "01234-567"
+                        ),
+                        Restaurant(
+                            id = 22,
+                            name = "Cozinha Africana",
+                            street = "Rua do Continente, 1010",
+                            city = "Sorocaba",
+                            postalCode = "12345-678"
+                        ),
+                        Restaurant(
+                            id = 23,
+                            name = "Mamma Mia!",
+                            street = "Rua da Nonna, 1111",
+                            city = "São José dos Campos",
+                            postalCode = "23456-789"
+                        ),
+                        Restaurant(
+                            id = 24,
+                            name = "Bistrô do Mar",
+                            street = "Avenida do Mar, 1212",
+                            city = "Santos",
+                            postalCode = "34567-890"
+                        ),
+                        Restaurant(
+                            id = 25,
+                            name = "Delícias da Roça",
+                            street = "Rua da Fazenda, 1313",
+                            city = "Ribeirão Preto",
+                            postalCode = "45678-901"
+                        ),
+                        Restaurant(
+                            id = 26,
+                            name = "Pizza & Pasta",
+                            street = "Rua do Molho, 1414",
+                            city = "Niterói",
+                            postalCode = "56789-012"
+                        ),
+                        Restaurant(
+                            id = 27,
+                            name = "Grill & Chill",
+                            street = "Avenida da Relaxação, 1515",
+                            city = "Brasília",
+                            postalCode = "67890-123"
+                        ),
+                        Restaurant(
+                            id = 28,
+                            name = "Piknik Café",
+                            street = "Rua da Diversão, 1616",
+                            city = "Piracicaba",
+                            postalCode = "78901-234"
+                        ),
+                        Restaurant(
+                            id = 29,
+                            name = "Bistrô Verde",
+                            street = "Rua da Sustentabilidade, 1717",
+                            city = "Juiz de Fora",
+                            postalCode = "89012-345"
+                        ),
+                        Restaurant(
+                            id = 30,
+                            name = "Feijoada do Chef",
+                            street = "Rua da Feijoada, 1818",
+                            city = "Belém",
+                            postalCode = "90123-456"
+                        ),
+                        Restaurant(
+                            id = 31,
+                            name = "Culinária Mediterrânea",
+                            street = "Avenida do Mediterrâneo, 1919",
+                            city = "Blumenau",
+                            postalCode = "01234-567"
+                        ),
+                        Restaurant(
+                            id = 32,
+                            name = "Restaurante 5 Estrelas",
+                            street = "Rua do Luxo, 2020",
+                            city = "São Carlos",
+                            postalCode = "12345-678"
+                        ),
+                        Restaurant(
+                            id = 33,
+                            name = "Pizzaria do Bairro",
+                            street = "Rua do Bairro, 2121",
+                            city = "São Vicente",
+                            postalCode = "23456-789"
+                        ),
+                        Restaurant(
+                            id = 34,
+                            name = "Churrasquinho de Rua",
+                            street = "Rua da Avenida, 2222",
+                            city = "São Bernardo do Campo",
+                            postalCode = "34567-890"
+                        ),
+                        Restaurant(
+                            id = 35,
+                            name = "Café da Manhã",
+                            street = "Avenida do Café, 2323",
+                            city = "São José",
+                            postalCode = "45678-901"
+                        ),
+                        Restaurant(
+                            id = 36,
+                            name = "Burguer Fino",
+                            street = "Rua do Gourmet, 2424",
+                            city = "Osasco",
+                            postalCode = "56789-012"
+                        ),
+                        Restaurant(
+                            id = 37,
+                            name = "Espaço Vegano",
+                            street = "Rua Verde, 2525",
+                            city = "Macaé",
+                            postalCode = "67890-123"
+                        ),
+                        Restaurant(
+                            id = 38,
+                            name = "Café da Tarde",
+                            street = "Avenida do Chá, 2626",
+                            city = "Araraquara",
+                            postalCode = "78901-234"
+                        ),
+                        Restaurant(
+                            id = 39,
+                            name = "Comida de Boteco",
+                            street = "Rua do Boteco, 2727",
+                            city = "São Caetano do Sul",
+                            postalCode = "89012-345"
+                        ),
+                        Restaurant(
+                            id = 40,
+                            name = "Bistrô Chic",
+                            street = "Rua da Elegância, 2828",
+                            city = "Niterói",
+                            postalCode = "90123-456"
+                        ),
+                        Restaurant(
+                            id = 41,
+                            name = "Pasta & Risotto",
+                            street = "Rua da Itália, 2929",
+                            city = "São Mateus",
+                            postalCode = "01234-567"
+                        ),
+                        Restaurant(
+                            id = 42,
+                            name = "Bar e Grill",
+                            street = "Rua do Bar, 3030",
+                            city = "São João de Meriti",
+                            postalCode = "12345-678"
+                        ),
+                        Restaurant(
+                            id = 43,
+                            name = "Restaurante Familiar",
+                            street = "Rua do Lar, 3131",
+                            city = "Santo André",
+                            postalCode = "23456-789"
+                        ),
+                        Restaurant(
+                            id = 44,
+                            name = "Culinária Brasileira",
+                            street = "Rua do Brasil, 3232",
+                            city = "Camaçari",
+                            postalCode = "34567-890"
+                        ),
+                        Restaurant(
+                            id = 45,
+                            name = "Restaurante da Vovó",
+                            street = "Rua da Memória, 3333",
+                            city = "Nova Iguaçu",
+                            postalCode = "45678-901"
+                        ),
+                        Restaurant(
+                            id = 46,
+                            name = "Pé na Areia",
+                            street = "Avenida da Praia, 3434",
+                            city = "Ilhéus",
+                            postalCode = "56789-012"
+                        ),
+                        Restaurant(
+                            id = 47,
+                            name = "Café com Leite",
+                            street = "Rua do Café, 3535",
+                            city = "Divinópolis",
+                            postalCode = "67890-123"
+                        ),
+                        Restaurant(
+                            id = 48,
+                            name = "Pasta e Pesto",
+                            street = "Rua do Pesto, 3636",
+                            city = "São Gabriel",
+                            postalCode = "78901-234"
+                        ),
+                        Restaurant(
+                            id = 49,
+                            name = "Taco e Tequila",
+                            street = "Rua do Tequila, 3737",
+                            city = "Petrolina",
+                            postalCode = "89012-345"
+                        ),
+                        Restaurant(
+                            id = 50,
+                            name = "Restaurante do Lago",
+                            street = "Rua do Lago, 3838",
+                            city = "Chapecó",
+                            postalCode = "90123-456"
+                        )
+                    )
+                )
+
+                restaurantDao.insertAll(restaurants)
+            }
+        }
 
         fun addFoodDataToDatabase(context: Context) {
             val foods = ArrayList<Food>()
