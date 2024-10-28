88e28058d3049f21473eb42cfe9d5721607ec601
Junior Oliveira <jo2025698@gmail.com>
2024-10-28 19:01:12 -0300

    refactor: remove interfaces

--- app/src/main/java/com/devmobile/android/restaurant/IOnCheckCheckbox.kt
+++
@@ -1,8 +0,0 @@
-package com.devmobile.android.restaurant
-
-import com.devmobile.android.restaurant.view.viewholders.FoodCardViewHolder
-
-interface IOnCheckCheckbox {
-
-    fun isCheckboxChecked(v: FoodCardViewHolder, isCheckboxChecked: Boolean)
-}
