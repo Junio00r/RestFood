2ecb43a9484e9bc09436a4cec732a6618eaccab1
Junior Oliveira <jo2025698@gmail.com>
2025-04-20 19:16:15 -0300

    feat: new bag item object

--- app/src/main/java/com/devmobile/android/restaurant/viewmodel/bottomnavigation/BagSharedViewModel.kt
+++ app/src/main/java/com/devmobile/android/restaurant/viewmodel/bottomnavigation/BagSharedViewModel.kt
@@ -17,11 +17,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class BagSharedViewModel(
-    val restaurantId: Long? = null,
     private val repository: BagRemoteRepository,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default,
     private val coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
@@ -33,7 +31,7 @@ class BagSharedViewModel(
     val wasItemAdded = _wasItemAdded.asStateFlow()
 
     // Need create a object bag for improve the code...
-    private val _itemsOnBag = MutableStateFlow<List<BagItem>>(emptyList())
+    private val _itemsOnBag = MutableStateFlow<List<BagRestaurantGroup>>(emptyList())
     val itemsOnBag = _itemsOnBag.asStateFlow()
 
     // Item selected
@@ -74,7 +72,7 @@ class BagSharedViewModel(
 
     fun fetchItem(restaurantId: Long, itemId: Long) {
 
-        if(_currentItem == null) {
+        if (_currentItem == null) {
 
             coroutineScope.launch {
                 repository.requestItem(restaurantId, itemId).also { item ->
@@ -96,25 +94,35 @@ class BagSharedViewModel(
         }
     }
 
-    fun addItemOnBag(itemId: Long? = null) {
+    fun addItemOnBag(restaurantId: Long? = null, itemId: Long? = null) {
 
         coroutineScope.launch {
             _wasItemAdded.value = RequestState.Loading
 
             _currentItem?.let { currentItem ->
+
                 val itemToInsert = BagItem(currentItem, _newRequiredSides.value)
+                val isThereThisRestaurantOnBag = _itemsOnBag.value.any { it.restaurantId == currentItem.restaurantId }
 
-                _itemsOnBag.update { currentList ->
+                if(isThereThisRestaurantOnBag) {
+                    _itemsOnBag.value
+                        .first { it.restaurantId == currentItem.restaurantId }
+                        .addItem(itemToInsert)
+                } else {
 
-                    if (!currentList.any { it.item.id == currentItem.id })
-                        currentList + itemToInsert
-                    else
-                        currentList
+                    _itemsOnBag.value
+                        .toMutableList()
+                        .add(
+                            BagRestaurantGroup(currentItem.restaurantId, mutableListOf(itemToInsert))
+                        )
                 }
 
+
+                // Reset states
                 _currentItem = null
                 _newRequiredSides.value = null
 
+                // Update request state
                 _wasItemAdded.value = RequestState.Success()
                 delay(200)
                 _wasItemAdded.value = null
@@ -122,11 +130,10 @@ class BagSharedViewModel(
         }
     }
 
-    fun removeItemOnBag(itemId: Long) {
+    fun removeItemOnBag(restaurantId: Long, itemId: Long) {
         coroutineScope.launch {
 
-            val listWithoutItem = _itemsOnBag.value.toMutableList().filter { it.item.id == itemId }
-            _itemsOnBag.value = listWithoutItem
+            _itemsOnBag.value.first { it.restaurantId == restaurantId }.removeItem(itemId)
         }
     }
 
@@ -138,17 +145,49 @@ class BagSharedViewModel(
     companion object {
 
         fun provideFactory(
-            restaurantId: Long? = null, repository: BagRemoteRepository
+            repository: BagRemoteRepository
         ): AbstractSavedStateViewModelFactory = object : AbstractSavedStateViewModelFactory() {
 
             override fun <T : ViewModel> create(
                 key: String, modelClass: Class<T>, handle: SavedStateHandle
             ): T {
 
-                return BagSharedViewModel(restaurantId, repository) as T
+                return BagSharedViewModel(repository) as T
             }
         }
     }
 }
 
 data class BagItem(val item: Item, val complementaryItems: List<ItemBetweenUiAndVM>? = null)
+
+data class BagRestaurantGroup(
+    val restaurantId: Long,
+    val itemsAdded: MutableList<BagItem> = mutableListOf()
+) {
+
+    fun addItem(newItem: BagItem): Boolean {
+        if (itemsAdded.contains(newItem)) {
+            return false
+        }
+
+        itemsAdded.add(newItem)
+
+        return true
+    }
+
+    fun removeItem(itemId: Long): Boolean {
+        return itemsAdded.removeIf { it.item.id == itemId }
+    }
+
+    fun updateItem(itemId: Long, newItem: BagItem): Boolean {
+
+        if (removeItem(itemId)) {
+            return addItem(newItem)
+        }
+        return false
+    }
+
+    fun size(): Int {
+        return itemsAdded.size
+    }
+}
